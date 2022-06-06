8ff2d0c6b938e91cdf86a564bbdf1449ffd49f5d
teetarus <victortarus6@gmail.com>
2022-06-06 14:19:35 +0300

    crude done

--- src/controllers/logoutController.js
+++ src/controllers/logoutController.js
@@ -1,14 +1,6 @@
-const usersDB = {
-  users: require("../model/users.json"),
-  setUsers: function (data) {
-    this.users = data;
-  },
-};
+const User = require("../model/User");
 
-const fsPromises = require("fs").promises;
-const path = require("path");
-
-const handleLogout = async(req, res) => {
+const handleLogout = async (req, res) => {
   //On client also delete the accessToken
   const cookies = req.cookies;
   if (!cookies?.jwt) return res.sendStatus(204); //No contet
@@ -16,25 +8,17 @@ const handleLogout = async(req, res) => {
   const refreshToken = cookies.jwt;
   //Is refreshToken in db?
 
-  const foundUser = usersDB.users.find(
-    (person) => person.refreshToken === refreshToken
-  );
+  const foundUser = await User.findOne({ refreshToken }).exec();
   if (!foundUser) {
-    res.clearCookie("jwt", { httpOnly: true, sameSite:'None',secure:true });
+    res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: true });
     return res.sendStatus(204);
   }
   //Delete refreshToken in db;
-  const otherUsers = usersDB.users.filter(
-    (person) => person.refreshToken !== foundUser.refreshToken
-    );
-    const currentUser = { ...foundUser, refreshToken: '' };
-    usersDB.setUsers([...otherUsers, currentUser]);
-    await fsPromises.writeFile(
-        path.join(__dirname, '..', 'model', 'users.json'),
-    JSON.stringify(usersDB.users)
-    )
-    res.clearCookie('jwt', { httpOnly: true,sameSite:'None',secure:true })
-    res.sendStatus(204);
+    foundUser.refreshToken = '';
+    const result = await foundUser.save(); //save changes to mongo db;
+    console.log(result); //delete after
+  res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: true });
+  res.sendStatus(204);
 };
 
 module.exports = { handleLogout };
